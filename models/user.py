2fff7666e07116035240b2452a9b6e63c598da0f
digi-21 <testmailhuyaar@gmail.com>
2024-01-30 21:20:45 +0530

    fix

--- models/user.py
+++ models/user.py
@@ -10,6 +10,21 @@ from typing import Union
 from fastapi import Body
 from bson.objectid import ObjectId as BsonObjectId
 
+class PydanticObjectId(BsonObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not isinstance(v, BsonObjectId):
+            v = v  # raise TypeError('ObjectId required')
+        return str(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
 class User :
     first_name : str | None = None
     last_name : str | None = None
