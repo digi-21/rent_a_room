5b2f3907e56ea1b602aed5a51919a2b7e350452b
digi-21 <testmailhuyaar@gmail.com>
2024-02-18 08:54:01 +0530

    3.2.1

--- routes/user.py
+++ routes/user.py
@@ -7,52 +7,10 @@ from fastapi.responses import JSONResponse
 router = APIRouter()
 
 #to add a new user 
-# @router.post("/add_a_user/")
-# async def add_user(request:Request, current_user: dict = Depends(get_current_user)):
-#     uuid = current_user.get("user_id")
-#     data = await request.json()
-#     first_name = data['first_name']
-#     last_name = data["last_name"]
-#     date_of_birth = data['date_of_birth']
-#     gender = data['gender']
-#     # email = data["email"]
-#     # phone_number = data["phone_number"]
-#     user: User = {
-#         "first_name" : first_name,
-#         "last_name" : last_name,
-#         "date_of_birth" : date_of_birth,
-#         "gender" : gender,
-#         "firebase_id" : uuid,
-#         "email" : None,
-#         "phone_number" : None,
-#         "profile_pic" : None,
-#         "space" : None,
-#         "Tenant" : None,
-#         "Buddyup" : None,
-#         "verification" : False, 
-#         "notifications" : None,
-#         "liked" : None
-#     }
-#     try:
-#         await add_new_user(user)
-#         response_data = {
-#         "status_code": "200",
-#         "message": "user created successfully!",
-#         }
-#         response = JSONResponse(content=response_data)
-#         return response
-    
-#     except:
-#         raise HTTPException(status_code=400, detail="Bad request")   
-    
-
-
-
 @router.post("/add_a_user/")
-async def add_user(request:Request):
-    print("1")
+async def add_user(request:Request, current_user: dict = Depends(get_current_user)):
+    uuid = current_user.get("user_id")
     data = await request.json()
-    print(data)
     first_name = data['first_name']
     last_name = data["last_name"]
     date_of_birth = data['date_of_birth']
@@ -64,7 +22,7 @@ async def add_user(request:Request):
         "last_name" : last_name,
         "date_of_birth" : date_of_birth,
         "gender" : gender,
-        "firebase_id" : None,
+        "firebase_id" : uuid,
         "email" : None,
         "phone_number" : None,
         "profile_pic" : None,
@@ -86,3 +44,45 @@ async def add_user(request:Request):
     
     except:
         raise HTTPException(status_code=400, detail="Bad request")   
+    
+
+
+
+# @router.post("/add_a_user/")
+# async def add_user(request:Request):
+#     print("1")
+#     data = await request.json()
+#     print(data)
+#     first_name = data['first_name']
+#     last_name = data["last_name"]
+#     date_of_birth = data['date_of_birth']
+#     gender = data['gender']
+#     # email = data["email"]
+#     # phone_number = data["phone_number"]
+#     user: User = {
+#         "first_name" : first_name,
+#         "last_name" : last_name,
+#         "date_of_birth" : date_of_birth,
+#         "gender" : gender,
+#         "firebase_id" : None,
+#         "email" : None,
+#         "phone_number" : None,
+#         "profile_pic" : None,
+#         "space" : None,
+#         "Tenant" : None,
+#         "Buddyup" : None,
+#         "verification" : False, 
+#         "notifications" : None,
+#         "liked" : None
+#     }
+#     try:
+#         await add_new_user(user)
+#         response_data = {
+#         "status_code": "200",
+#         "message": "user created successfully!",
+#         }
+#         response = JSONResponse(content=response_data)
+#         return response
+    
+#     except:
+#         raise HTTPException(status_code=400, detail="Bad request")   
